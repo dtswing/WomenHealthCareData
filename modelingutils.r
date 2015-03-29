43da655959993dcc3b980b4e37250e2804a957e5
Siying Chen <sc3493@columbia.edu>
2015-03-29 15:41:59 -0400

    Added imputation, random forest model, and k fold validation

---
+++ modelingutils.r
@@ -0,0 +1,18 @@
+calculateAUC_rf<-function(model,build,bad){
+  b<-build
+  b$predicted<-as.numeric(predict(model,type="prob",build)[,2])
+  b$actual<-as.numeric(b[,bad])-1
+  b$errors<-b$actual-b$predicted
+  mse<-mean(b$errors^2)
+  
+  b$predict<- predict(model,b, type = "prob")
+  b$predict2<- floor(b$predict[,2]*10)/10
+  #xtabs(~predict2+bad,data=b)
+  
+  pred_obj = prediction(b$predict[,2], b[,bad])
+  perf <- performance(pred_obj, measure = "tpr", x.measure = "fpr")
+  #plot(perf, col=rainbow(10)) #red line plot
+  auc = round(performance(pred_obj, "auc")@y.values[[1]], digits=3)
+  #print(paste("Test set AUC:",auc))
+  auc
+}
--- women_healthcare.R
+++ women_healthcare.R
@@ -1,9 +1,13 @@
 setwd("~/Siying/WomenHealthCareData")
+source("modelingutils.r")
 
 #load libraries
 library(data.table)
 library(dismo)
 library(Amelia)
+library(randomForest)
+library(ROCR)
+library(caret)
 
 #input datasets
 train.values <- read.csv('train_values.csv')
@@ -11,8 +15,8 @@ train.labels <- read.csv('train_labels.csv')
 test.values <- read.csv('test_values.csv')
 
 #traning set
-train.combined <- cbind(train.labels[,2],train.values[,-1])
-colnames(train.combined) <- c(names(train.labels)[2],names(train.values)[-1])
+train.combined <- cbind(train.labels,train.values)
+train.combined$id <- NULL
 
 #remove variables with only missing values
 train.combined <- train.combined[,colSums(is.na(train.combined) | train.combined == 0)<nrow(train.combined)]
@@ -24,17 +28,15 @@ train.combined <- subset(train.combined, select=uniquelength>1)
 #summary of training set
 summary(train.combined)
 
-#Small training set with no missing values
-train.combined.nonmissing <- na.omit(train.combined)
-
 #Generate dataset excluding variables with x% of missing values
-train.combined.v2 <- train.combined[,colSums(is.na(train.combined) | train.combined == 0 | train.combined == '') < nrow(train.combined)*0.6] 
+train.combined.v2 <- train.combined[,colSums(is.na(train.combined)| train.combined == '') < nrow(train.combined)*0.6] 
 
 #Data imputation
-#Define varialbes for imputation
 colNumeric <- names(train.combined.v2)[grepl('n_', names(train.combined.v2))]
 colOrdinal <- names(train.combined.v2)[grepl('o_', names(train.combined.v2))]
 colCategorical <- names(train.combined.v2)[grepl('c_', names(train.combined.v2))]
+colRelease <- names(train.combined.v2)[grepl('release', names(train.combined.v2))]
+colService <- names(train.combined.v2)[grepl('service_', names(train.combined.v2))]
 
 #Take empty categorical variables as NAs
 train.combined.v2[,colCategorical] <- lapply(train.combined.v2[,colCategorical], function(x) ifelse(is.na(x)|x=='','NA',x))
@@ -48,15 +50,62 @@ train.imputed <- amelia(train.impute.prep,ords = colOrdinal, m=5, incheck = FALS
 
 save(train.imputed, file = "imputationsNumericOrdinalOnly.RData") 
 
-#Naive logsitic regression
+train.combined.v2[,colService] <- lapply(train.combined.v2[,colService], factor)
+train.combined.v2$release <- as.factor(train.combined.v2$release)
+train.combined.v2$id <- NULL
 
 #Random Forest 
 
+#K-fold validation
+folds <- createFolds(train.set$service_a, k=20)
+split_up <- lapply(folds, function(ind, dat) dat[ind,], dat = train.set)
+
+aucs <- NULL
+for (i in 1:10){
+  test <- split_up[[i]]
+  train <- train.set[-as.numeric(row.names(test)),]
+  rf_fit <- randomForest(service_a~., data=train, maxnodes=20, importance=TRUE, na.action=na.omit)
+  
+  pred_train <- predict(rf_fit, train, type = "prob")[,2]
+  pred_obj_train <- prediction(pred_train, train$service_a)
+  
+  pred_test <- predict(rf_fit, test, type = "prob")[,2]
+  pred_obj_test <- prediction(pred_test, test$service_a)
+  
+  aucs <- rbind(aucs,c(round(performance(pred_obj_test, "auc")@y.values[[1]], digits=3), round(performance(pred_obj_train, "auc")@y.values[[1]], digits=3)))
+}
+
+aucs <- data.frame(aucs)
+names(aucs) <- c("validate_auc", "build_auc")
+print(paste0("validate: ", median(aucs$validate_auc), "   build: ",median(aucs$build_auc)))
+
+
+#fit on all observastions
+
+train.set <- cbind(train.combined.v2[,colService], train.combined.v2$release, train.imputed$imputations[[1]],train.combined.v2[,colCategorical])
+colnames(train.set) <- c(colService,colRelease,names(train.imputed$imputations[[1]]),colCategorical)
+
+y_pred <- NULL
+auc_pred <- NULL
+fit_list <- NULL
+
+for (y in colService){
+rf_fit <- randomForest(train.set[,y] ~., data=train.set[,names(train.set)[!names(train.set) %in% colService]], maxnodes=20, importance=TRUE, na.action=na.omit)
+fit_list <-cbind(fit_list, rf_fit)
+pred_train_all <- predict(rf_fit, train.set, type = "prob")[,2]
+pred_obj_train_all <- prediction(pred_train_all, train.set$service_a)
+auc_train_all <- round(performance(pred_obj_train_all, "auc")@y.values[[1]], digits=3)
+y_pred <- cbind(y_pred, pred_obj_train_all)
+auc_pred <- cbind(auc_pred,auc_train_all)
+} 
+
+#GLM
+
 #Gradient Boosted Tree
-build.tc5.lr01 <- gbm.step(data=train.combined.nonmissing, gbm.x = names(train.combined.nonmissing)[names(train.combined.nonmissing)!="service_a"], gbm.y = "service_a",
+#build.tc5.lr01 <- gbm.step(data=train.combined.nonmissing, gbm.x = names(train.combined.nonmissing)[names(train.combined.nonmissing)!="service_a"], gbm.y = "service_a",
                            family = "bernoulli", tree.complexity = 5,
                            learning.rate = 0.01, bag.fraction = 0.5)
-summary(build.tc5.lr01)
+#summary(build.tc5.lr01)
 
 #Naives Bayes
 
