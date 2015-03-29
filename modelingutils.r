f1ea925ec347020cb745fa8c3c6ce5d78fbea7af
Siying Chen <sc3493@columbia.edu>
2015-03-29 17:04:55 -0400

    added logloss function

--- modelingutils.r
+++ modelingutils.r
@@ -1,3 +1,8 @@
+LogLoss <- function(actual, predicted, eps=0.00001) {
+  predicted <- pmin(pmax(predicted, eps), 1-eps)
+  -1/length(actual)*(sum(actual*log(predicted)+(1-actual)*log(1-predicted)))
+}
+
 calculateAUC_rf<-function(model,build,bad){
   b<-build
   b$predicted<-as.numeric(predict(model,type="prob",build)[,2])
--- women_healthcare.R
+++ women_healthcare.R
@@ -29,7 +29,7 @@ train.combined <- subset(train.combined, select=uniquelength>1)
 summary(train.combined)
 
 #Generate dataset excluding variables with x% of missing values
-train.combined.v2 <- train.combined[,colSums(is.na(train.combined)| train.combined == '') < nrow(train.combined)*0.6] 
+train.combined.v2 <- train.combined[,colSums(is.na(train.combined)| train.combined == '') < nrow(train.combined)*0.7] 
 
 #Data imputation
 colNumeric <- names(train.combined.v2)[grepl('n_', names(train.combined.v2))]
@@ -39,14 +39,14 @@ colRelease <- names(train.combined.v2)[grepl('release', names(train.combined.v2)
 colService <- names(train.combined.v2)[grepl('service_', names(train.combined.v2))]
 
 #Take empty categorical variables as NAs
-train.combined.v2[,colCategorical] <- lapply(train.combined.v2[,colCategorical], function(x) ifelse(is.na(x)|x=='','NA',x))
+train.combined.v2[,colCategorical] <- lapply(train.combined.v2[,colCategorical], function(x) ifelse(is.na(x)|x=='',NA,x))
 train.combined.v2[,colCategorical] <- lapply(train.combined.v2[,colCategorical], factor)
 summary(train.combined.v2[,colCategorical])
 
 #Impute numerical and oridanl variables
-train.impute.prep <- train.combined.v2[,c(colNumeric, colOrdinal)]
-train.impute.prep <- train.impute.prep[rowSums(is.na(train.impute.prep) | train.impute.prep == 0)<ncol(train.impute.prep),]
-train.imputed <- amelia(train.impute.prep,ords = colOrdinal, m=5, incheck = FALSE)
+train.impute.prep <- train.combined.v2[,c(colNumeric, colOrdinal, colCategorical, colRelease)]
+train.impute.prep <- train.impute.prep[rowSums(is.na(train.impute.prep) | train.impute.prep == '')<ncol(train.impute.prep),]
+train.imputed <- amelia(train.impute.prep, noms = c(colRelease,colCategorical) ords = colOrdinal,m=5, incheck = FALSE)
 
 save(train.imputed, file = "imputationsNumericOrdinalOnly.RData") 
 
@@ -89,13 +89,14 @@ y_pred <- NULL
 auc_pred <- NULL
 fit_list <- NULL
 
-for (y in colService){
+for (y in c('service_a','service_b','service_c')){
 rf_fit <- randomForest(train.set[,y] ~., data=train.set[,names(train.set)[!names(train.set) %in% colService]], maxnodes=20, importance=TRUE, na.action=na.omit)
-fit_list <-cbind(fit_list, rf_fit)
+fit_list <- list(fit_list, rf_fit)
 pred_train_all <- predict(rf_fit, train.set, type = "prob")[,2]
-pred_obj_train_all <- prediction(pred_train_all, train.set$service_a)
+pred_obj_train_all <- prediction(pred_train_all, train.set[,y])
 auc_train_all <- round(performance(pred_obj_train_all, "auc")@y.values[[1]], digits=3)
-y_pred <- cbind(y_pred, pred_obj_train_all)
+logloss_train <- LogLoss(as.numeric(train.set[,y])-1,pred_train_all)
+y_pred <- cbind(y_pred, pred_train_all)
 auc_pred <- cbind(auc_pred,auc_train_all)
 } 
 
@@ -103,8 +104,8 @@ auc_pred <- cbind(auc_pred,auc_train_all)
 
 #Gradient Boosted Tree
 #build.tc5.lr01 <- gbm.step(data=train.combined.nonmissing, gbm.x = names(train.combined.nonmissing)[names(train.combined.nonmissing)!="service_a"], gbm.y = "service_a",
-                           family = "bernoulli", tree.complexity = 5,
-                           learning.rate = 0.01, bag.fraction = 0.5)
+#                           family = "bernoulli", tree.complexity = 5,
+#                           learning.rate = 0.01, bag.fraction = 0.5)
 #summary(build.tc5.lr01)
 
 #Naives Bayes
